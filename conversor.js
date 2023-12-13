7790d2b44b9911ea840a5945bf2c194f38b81f9e
unknown <raphafm.rf@gmail.com>
2023-12-13 17:52:51 -0300

    estilizacoes em desafios e tradutor

--- conversor.js
+++ conversor.js
@@ -1,34 +1,36 @@
 let isAsciiToBinary = true;
 
-    function convertText() {
-      const input = document.getElementById('inputText').value;
-      const resultDiv = document.getElementById('result');
+function convertText() {
+  const input = document.getElementById('inputText').value;
+  const resultDiv = document.getElementById('result');
 
-      let convertedResult = '';
+  let convertedResult = '';
 
-      if (isAsciiToBinary) {
-        // Converte ASCII para binário
-        for (let i = 0; i < input.length; i++) {
-          const charCode = input.charCodeAt(i);
-          const binary = charCode.toString(2).padStart(8, '0'); // Converte para binário de 8 dígitos
-          convertedResult += binary + ' ';
-        }
-        resultDiv.textContent = 'Texto em binário: ' + convertedResult;
-      } else {
-        // Converte binário para ASCII
-        const binaryArray = input.split(' ');
-        for (let i = 0; i < binaryArray.length; i++) {
-          const decimal = parseInt(binaryArray[i], 2);
-          const asciiChar = String.fromCharCode(decimal);
-          convertedResult += asciiChar;
-        }
-        resultDiv.textContent = 'Texto em ASCII: ' + convertedResult;
-      }
+  if (isAsciiToBinary) {
+    // Converte ASCII para binário
+    for (let i = 0; i < input.length; i++) {
+      const charCode = input.charCodeAt(i);
+      const binary = charCode.toString(2).padStart(8, '0'); // Converte para binário de 8 dígitos
+      convertedResult += binary + ' ';
     }
+    resultDiv.textContent = 'Texto em binário: ' + convertedResult;
+  } else {
+    // Converte binário para ASCII
+    const binaryArray = input.split(' ');
+    for (let i = 0; i < binaryArray.length; i++) {
+      const decimal = parseInt(binaryArray[i], 2);
+      const asciiChar = String.fromCharCode(decimal);
+      convertedResult += asciiChar;
+    }
+    resultDiv.textContent = 'Texto em ASCII: ' + convertedResult;
+  }
+}
 
-    function toggleConversion() {
-        isAsciiToBinary = !isAsciiToBinary;
-        const button = document.getElementById('toggleButton');
-        button.textContent = isAsciiToBinary ? 'Inverter (Ascii -> Binario)' : 'Inverter (Binario -> Ascii)';
-        document.getElementById('result').textContent = '';
-      }
+function toggleConversion() {
+    isAsciiToBinary = !isAsciiToBinary;
+    const button = document.getElementById('toggleButton');
+    const convertButton = document.getElementById('convertButton');
+    button.textContent = isAsciiToBinary ? 'Inverter (Ascii -> Binario)' : 'Inverter (Binario -> Ascii)';
+    convertButton.textContent = isAsciiToBinary ? 'Converter para Binário' : 'Converter para Ascii';
+    document.getElementById('result').textContent = '';
+  }
